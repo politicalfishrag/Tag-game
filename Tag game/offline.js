48afe2c7ffe97b5ea600648be3e53f43514ed67e
politicalfishrag <50838496+politicalfishrag@users.noreply.github.com>
2019-05-20 19:30:42 -0400

    tag game files

---
+++ Tag game/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1558363384,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player1-sheet0.png",
+		"images/player1-sheet1.png",
+		"images/player2-sheet0.png",
+		"images/player2-sheet1.png",
+		"images/sprite-sheet0.png",
+		"images/greenwin-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/redlose-sheet0.png",
+		"images/redwin-sheet0.png",
+		"images/greenlose-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
